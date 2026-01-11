2b04ad518a8c00c613c51430eebbc13b8d80a9f5
ENGO150 <engo@satan.red>
2026-01-11 14:28:47 +0100

    move main::run to lib

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::Builder;
-
-use why2_desktop_lib::network;
-
 fn main()
 {
-    Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![network::try_connect])
-        .run(tauri::generate_context!())
-        .expect("Failed to run app");
+    why2_desktop_lib::run();
 }
