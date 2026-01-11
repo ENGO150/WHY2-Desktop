42adb9fa001197bb9aab3df3aba96991572f5212
ENGO150 <engo@satan.red>
2026-01-11 12:58:07 +0100

    display connection message in ServerView

--- src-tauri/src/network.rs
+++ src-tauri/src/network.rs
@@ -1,4 +1,4 @@
-use tauri::AppHandle;
+use tauri::{ AppHandle, Emitter };
 
 use std::
 {
@@ -8,17 +8,59 @@ use std::
     sync::mpsc::{ self, Receiver },
 };
 
+use serde::Serialize;
+
 use why2::chat::
 {
     config,
     network::client::{ self, ClientEvent },
 };
 
+//STRUCTS
+#[derive(Clone, Serialize)]
+struct FrontendPayload
+{
+    event_type: String,
+    content: String,
+    username: Option<String>,
+    extra: Option<String>,
+    clear_count: Option<usize>,
+}
+
 //PRIVATE
 fn handle_client_events(rx: Receiver<ClientEvent>, app: AppHandle)
 {
     while let Ok(event) = rx.recv()
     {
+        let payload = match event
+        {
+            ClientEvent::Connected(server_name) =>
+            {
+                //UPDATE HEADER
+                let _ = app.emit("server-payload", FrontendPayload
+                {
+                    event_type: "status".into(),
+                    content: "".into(),
+                    username: None,
+                    extra: Some(server_name.clone()), // This string goes to Header
+                    clear_count: None,
+                });
+
+                //PRINT LOG
+                FrontendPayload
+                {
+                    event_type: "info".into(),
+                    content: format!("Successfully connected to {}.\n", server_name),
+                    username: None,
+                    extra: None,
+                    clear_count: None,
+                }
+            },
+
+            _ => continue
+        };
+
+        let _ = app.emit("server-payload", payload);
     }
 }
 
